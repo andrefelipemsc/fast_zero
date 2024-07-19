ddd5256f9c39931f5415d22428ceab53ab57fc3e
André Felipe <andrefelipemsc@gmail.com>
2024-07-19 20:01:55 -0300

    adicionando mais testes e rota para refresh token

--- tests/test_users.py
+++ tests/test_users.py
@@ -55,6 +55,21 @@ def test_update_user(client, user, token):
     }
 
 
+def test_update_user_with_wrong_user(client, other_user, token):
+    response = client.put(
+        f'/users/{other_user.id}',
+        headers={'Authorization': f'Bearer {token}'},
+        json={
+            'username': 'new_username',
+            'email': 'new_email@test.com',
+            'password': 'new_password',
+        },
+    )
+
+    assert response.status_code == HTTPStatus.FORBIDDEN
+    assert response.json() == {'detail': 'Not enough permission'}
+
+
 def test_delete_user(client, user, token):
     response = client.delete(
         f'/users/{user.id}',
@@ -64,9 +79,9 @@ def test_delete_user(client, user, token):
     assert response.json() == {'message': 'User deleted'}
 
 
-def test_delete_wrong_user(client, user, token):
+def test_delete_wrong_user(client, other_user, token):
     response = client.delete(
-        f'/users/{user.id + 1}',
+        f'/users/{other_user.id}',
         headers={'Authorization': f'Bearer {token}'},
     )
     assert response.status_code == HTTPStatus.FORBIDDEN
